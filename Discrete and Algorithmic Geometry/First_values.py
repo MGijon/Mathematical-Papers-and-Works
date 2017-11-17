1ac330a18ccfa77c0a4a0d9daf43b97ff0112025
Unknown <mgijon94@gmail.com>
2017-11-17 12:22:12 +0100

    DAG
    still working, first images

--- Discrete and Algorithmic Geometry/First_values.py
+++ Discrete and Algorithmic Geometry/First_values.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+## Data
+## ====
 x = [i for i in range(1, 101)]
 
 lower_bound = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6,
@@ -12,7 +14,6 @@ upper_bound = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6,
  				14, 15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20,
  				20, 20, 21, 21, 21, 21, 22, 22, 22, 22, 23, 23, 23, 23, 24, 24, 24, 24, 25, 25, 25, 25]
 
-print(len(upper_bound))
 
 known_values = [lower_bound[i] for i in range(0, len(lower_bound)) if lower_bound[i] == upper_bound[i]]
 
@@ -27,10 +28,11 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(x, lower_bound, color = 'blue', label = 'Lower Bounds')
 ax.plot(x, upper_bound, color = 'red', label = 'Upper Bounds')
-plt.title(r'Comparation between lower and upper bounds for $K_{n}$')
+plt.title(r'Comparation between lower and upper bounds for $\bar \theta (K_{n})$')
 ax.set_xlabel(r'Value of $n$')
-ax.set_ylabel('Bounds')
+ax.set_ylabel('Bound')
 plt.legend()
+plt.savefig('Comparation.png')
 plt.show()
 
 
@@ -48,12 +50,13 @@ ax.scatter(x[:20], upper_bound[:20], color = 'pink', label = 'Upper Bounds')
 
 ax.scatter([i for i in range(1, len(known_values) + 1)], known_values, marker = '^', color = 'red', label = 'Known values')
 
-plt.title(r'Comparation between lowe and upper bounds for $K_{n}$')
+plt.title(r'Known values of lower and upper bounds for $\bar \theta (K_{n})$')
 ax.set_xlabel(r'Value of $n$')
-ax.set_ylabel('Bounds')
+ax.set_ylabel('Bound')
 
 #plt.setp(xticklabels = [i for i in range(1, len(known_values) + 1)])
 plt.legend()
+plt.savefig('Known common values.png')
 plt.show()
 
 
